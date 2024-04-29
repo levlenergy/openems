278f8d202dc9256ef304286e25641794c56fd3a9
Luca Mancilik <luca.mancilik@levl.energy>
2024-04-29 17:10:28 +0200

    levl-891/978: fix checkstyle errors in levl simulator

--- io.openems.edge.levl.simulator/src/io/openems/edge/levl/simulator/OneWeekDataContainer.java
+++ io.openems.edge.levl.simulator/src/io/openems/edge/levl/simulator/OneWeekDataContainer.java
@@ -3,16 +3,36 @@ package io.openems.edge.levl.simulator;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * This class represents a container that holds data for one week.
+ * It extends the DataContainer class.
+ */
 public class OneWeekDataContainer extends DataContainer {
 
-    public  static  OneWeekDataContainer of( DataContainer container) {
+    /**
+     * Factory method to create a new instance of OneWeekDataContainer.
+     *
+     * @param container The DataContainer instance.
+     * @return a new instance of OneWeekDataContainer.
+     */
+    public  static  OneWeekDataContainer of(DataContainer container) {
         return new OneWeekDataContainer(container.records);
     }
 
+    /**
+     * Constructor for the OneWeekDataContainer.
+     *
+     * @param records The list of records to be stored in the container.
+     */
     public OneWeekDataContainer(List<Float[]> records) {
         super(records);
     }
 
+    /**
+     * This method sets the current index to the value corresponding to the current time.
+     *
+     * @param now The current date and time.
+     */
     public void setIndexToCurrentValue(LocalDateTime now) {
         int secondsPerMinute = 60;
         int secondsPerHour = 60 * secondsPerMinute;
